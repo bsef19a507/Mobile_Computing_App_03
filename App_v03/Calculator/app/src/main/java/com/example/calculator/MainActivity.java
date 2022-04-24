576ffd0ac9ba3b2c52688b2378d68d85ec7f1b78
redmoon <bsef19a507@pucit.edu.pk>
2022-04-24 22:45:48 +0500

    App version 0.1

---
+++ App_v03/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.calculator;
+
+import static android.view.View.*;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        Button button2 = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,login.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,aboutme.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Mobile_Computing_App_03
+# This is My 3rd Android App version 0.1
+[embed]App_v03/Pdf/calculator.pdf[/embed]
