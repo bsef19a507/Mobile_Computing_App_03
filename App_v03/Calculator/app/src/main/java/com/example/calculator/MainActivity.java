6faee3bd84f77e830e63eeb62773c4b2b154ff86
redmoon <bsef19a507@pucit.edu.pk>
2022-05-30 22:36:06 +0500

    calculate method code implementation

--- App_v03/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
+++ App_v03/Calculator/app/src/main/java/com/example/calculator/MainActivity.java
@@ -38,8 +38,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void calculate(View v){
-        EditText num1_str = findViewById(R.id.second_num);
-        EditText num2_str = findViewById(R.id.first_num);
+        EditText e1 = findViewById(R.id.second_num);
+        EditText e2 = findViewById(R.id.first_num);
+        String num1_str =e1.getText().toString();
+        String num1_str =e2.getText().toString();
         EditText result = findViewById(R.id.second_num2);
         int num1 = Integer.parseInt(num1_str);
         int num2 = Integer.parseInt(num2_str);
