8d49c37cf539f44ca9f575d3ffe4afb39dc63d20
redmoon <bsef19a507@pucit.edu.pk>
2022-05-30 23:03:57 +0500

    back button on about me screen implementation

--- App_v03/Calculator/app/src/main/java/com/example/calculator/aboutme.java
+++ App_v03/Calculator/app/src/main/java/com/example/calculator/aboutme.java
@@ -3,6 +3,7 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class aboutme extends AppCompatActivity {
 
@@ -10,5 +11,9 @@ public class aboutme extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_aboutme);
+    }
+    public void backToCalculator(View v){
+        
+
     }
 }
